0a660dcc21ddbab31290e623fb0e7d4ae2e472fb
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 20:47:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
+def reverse_each_word(string)
+  reversedString = string.split(" ").collect do |word|
   word.reverse
 end
-  reversed.join(" ")
+  reversedString.join(" ")
 end
