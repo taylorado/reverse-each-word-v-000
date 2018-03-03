6da4e4ac285a6fbfbfc51f23514cafb9b02ef0b8
Taylor Donlan <taylordonlan@gmail.com>
2018-03-03 20:42:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ def reverse_each_word(string)
   wordArray = Array.new
   wordArray = string.split(' ')
 
-  wordArray.each do |i|
-    wordArray[i].reverse
-  end
+  wordArray.each do |i| {wordArray[i].reverse}
+  
 
 newString = wordArray.join(" ")
   return newString
